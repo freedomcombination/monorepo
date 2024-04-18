bf604179abb28e401b4d3e891ffc235e7487a3b9
7alip <talipaltas@gmail.com>
2024-04-18 18:45:05 +0200

    Fix hashtag announcement caps

--- packages/ui/src/components/Caps/Caps.tsx
+++ packages/ui/src/components/Caps/Caps.tsx
@@ -4,9 +4,9 @@ import {
   chakra,
   Flex,
   forwardRef,
-  HStack,
   ImageProps,
   Skeleton,
+  Stack,
   Text,
   useMergeRefs,
 } from '@chakra-ui/react'
@@ -193,7 +193,7 @@ export const Caps = forwardRef<CapsProps, 'div'>(
               </chakra.svg>
 
               {/* Content */}
-              <HStack
+              <Stack
                 pos={'absolute'}
                 top={dimensions.padding}
                 bottom={dimensions.padding}
@@ -242,13 +242,12 @@ export const Caps = forwardRef<CapsProps, 'div'>(
                   fontSize={`${dimensions.font}px`}
                   overflow={'hidden'}
                   whiteSpace={'pre-wrap'}
-                  textAlign={flip ? 'right' : 'left'}
                   flexGrow={1}
                   alignItems={'center'}
                 >
                   {text}
                 </Text>
-              </HStack>
+              </Stack>
             </Flex>
           ) : (
             <Skeleton />
