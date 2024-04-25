2170643c7b18bd108d0b703e307d4c12dc917c8f
Ertugrul Aydin <ertugrulaydin81@gmail.com>
2024-04-25 23:35:10 +0200

    Link and color is changed in NotFound component

--- packages/ui/src/components/NotFound/NotFound.tsx
+++ packages/ui/src/components/NotFound/NotFound.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react'
 
-import { Box, Center, Heading, Link, Text } from '@chakra-ui/react'
+import { Box, Center, Heading, Text } from '@chakra-ui/react'
+import Link from 'next/link'
 import { useTranslation } from 'next-i18next'
 
 export const NotFound: FC = () => {
@@ -13,7 +14,7 @@ export const NotFound: FC = () => {
           {t('not-found')}
         </Heading>
         <Text mb={4}>{t('not-found-desc')}</Text>
-        <Link href="/" color="blue.500">
+        <Link href="/" color="primary.500">
           {t('not-found-button')}
         </Link>
       </Box>
