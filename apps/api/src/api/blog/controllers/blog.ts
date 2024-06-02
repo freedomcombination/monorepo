4ef5d1e49c26111d76ee4c7d8e24d6c7615acf83
7alip <talipaltas@gmail.com>
2024-06-02 13:27:00 +0200

    Refactor blog findOne controller

--- apps/api/src/api/blog/controllers/blog.ts
+++ apps/api/src/api/blog/controllers/blog.ts
@@ -4,36 +4,30 @@ import { factories } from '@strapi/strapi'
 export default factories.createCoreController('api::blog.blog', () => {
   return {
     async findOne(ctx) {
-      const { id } = ctx.params
-      const searchValue = id ? id : ctx.params.slug
-      const searchKey = id ? 'id' : 'slug'
+      const { id, slug } = ctx.params
 
-      const blog = await strapi.db.query('api::blog.blog').findOne({
-        where: { [searchKey]: searchValue },
-        populate: ['author', 'image'],
-      })
+      const blog = (await strapi.db.query('api::blog.blog').findOne({
+        where: id ? { id: id } : { slug: slug },
+        populate: { author: true, image: true },
+      })) as Blog
 
       const user = ctx.state.user
-      const sanitizedBlog = (await this.sanitizeOutput(blog, ctx)) as Blog
-
-      if (id) return sanitizedBlog
-
       const isLiked =
         user &&
         (await strapi.db.query('api::blog.blog').count({
           where: {
-            slug: searchValue,
-            likers: {
-              email: user.email,
-            },
+            slug,
+            likers: { email: user.email },
           },
-          select: ['id'],
         }))
 
-      return {
-        ...blog,
-        isLiked: !!isLiked,
-      }
+      blog.isLiked = !!isLiked
+
+      const sanitizedBlog = await this.sanitizeOutput(blog, ctx)
+
+      console.log('sanitizedBlog', sanitizedBlog)
+
+      return sanitizedBlog
     },
     async find(ctx) {
       const response = await super.find(ctx)
