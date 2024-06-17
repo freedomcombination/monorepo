e98efe0d47d3120d89b7828a7b6252f49f8bd57a
7alip <talipaltas@gmail.com>
2024-06-17 09:42:23 +0200

    Update pwa configs

--- packages/services/src/worker.ts
+++ packages/services/src/worker.ts
@@ -1,35 +1,39 @@
 const self = {} as ServiceWorkerGlobalScope
 
-self.addEventListener('push', event => {
-  const data = JSON.parse(event.data?.text() ?? '{ title: "" }')
-  event.waitUntil(
-    self.registration.showNotification(data.title, {
-      body: data.message,
-      icon: '/icons/android-chrome-192x192.png',
-    }),
-  )
-})
+if (typeof self.addEventListener === 'function') {
+  self.addEventListener('push', event => {
+    const data = JSON.parse(event.data?.text() ?? '{ title: "" }')
+    event.waitUntil(
+      self.registration.showNotification(data.title, {
+        body: data.message,
+        icon: '/icons/android-chrome-192x192.png',
+      }),
+    )
+  })
 
-self.addEventListener('notificationclick', event => {
-  event.notification.close()
-  event.waitUntil(
-    self.clients
-      .matchAll({ type: 'window', includeUncontrolled: true })
-      .then(clientList => {
-        if (clientList.length > 0) {
-          let client = clientList[0]
-          for (let i = 0; i < clientList.length; i++) {
-            if (clientList[i].focused) {
-              client = clientList[i]
+  self.addEventListener('notificationclick', event => {
+    event.notification.close()
+    event.waitUntil(
+      self.clients
+        .matchAll({ type: 'window', includeUncontrolled: true })
+        .then(clientList => {
+          if (clientList.length > 0) {
+            let client = clientList[0]
+            for (let i = 0; i < clientList.length; i++) {
+              if (clientList[i].focused) {
+                client = clientList[i]
+              }
             }
+
+            return client.focus()
           }
 
-          return client.focus()
-        }
+          return self.clients.openWindow('/')
+        }),
+    )
+  })
+} else {
+  console.error('self.addEventListener is not a function', self)
+}
 
-        return self.clients.openWindow('/')
-      }),
-  )
-})
-
-export type {}
+export default self
