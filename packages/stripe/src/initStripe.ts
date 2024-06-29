5072aea2a6ce99212346ddbe46e8b93cfe68f516
7alip <talipaltas@gmail.com>
2024-06-29 22:24:21 +0200

    Update packages

--- packages/stripe/src/initStripe.ts
+++ packages/stripe/src/initStripe.ts
@@ -3,5 +3,5 @@ import Stripe from 'stripe'
 import { getSecret } from '@fc/secrets'
 
 export const stripe = new Stripe(getSecret('STRIPE_KEY') as string, {
-  apiVersion: '2024-04-10',
+  apiVersion: '2024-06-20',
 })
