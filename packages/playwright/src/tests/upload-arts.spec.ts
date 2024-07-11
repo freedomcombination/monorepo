0c1f1cc538e383def6c1470156a41336a0cc955a
sehrigokcan <sehrigokcan@gmail.com>
2024-07-11 15:59:28 +0200

    Add describe block

--- packages/playwright/src/tests/upload-arts.spec.ts
+++ packages/playwright/src/tests/upload-arts.spec.ts
@@ -5,101 +5,100 @@ import { PASSWORD, USERNAME } from '../constants'
 import { HomePage, LoginPage } from '../pages'
 import { getVercelUrl } from '../utils'
 
-test('should not upload art without logging in', async ({ page }) => {
-  await page.goto(getVercelUrl('kunsthalte'))
+test.describe('Upload Arts', () => {
+  test('TC01- should not upload art without logging in', async ({ page }) => {
+    await page.goto(getVercelUrl('kunsthalte'))
 
-  await page.getByRole('link', { name: 'Arts' }).first().click()
-  await page.getByText('Upload Art').click()
-  const warning = page.locator('.chakra-modal__body p')
-  await page.waitForTimeout(1000)
-  expect(warning).toContainText(
-    'You must be logged in in order to be able to upload an art!',
-  )
-})
+    await page.getByRole('link', { name: 'Arts' }).first().click()
+    await page.getByText('Upload Art').click()
+    const warning = page.locator('.chakra-modal__body p')
+    await page.waitForTimeout(1000)
+    expect(warning).toContainText(
+      'You must be logged in in order to be able to upload an art!',
+    )
+  })
 
-test('should upload art with logging in', async ({ page }) => {
-  const loginPage = new LoginPage(page)
-  const homePage = new HomePage(page, 'kunsthalte')
+  test('TC02- should upload art with logging in', async ({ page }) => {
+    const loginPage = new LoginPage(page)
+    const homePage = new HomePage(page, 'kunsthalte')
 
-  await page.goto(homePage.url, { waitUntil: 'domcontentloaded' })
-  await homePage.gotoLogin()
-  await loginPage.login(USERNAME, PASSWORD)
-  await page.getByRole('link', { name: 'Arts' }).first().click()
-  await page.getByText('Upload Art').click()
-  await page.waitForTimeout(1000)
-  expect(page.locator('.uppy-Dashboard-AddFiles-title')).toBeVisible()
-})
+    await page.goto(homePage.url, { waitUntil: 'domcontentloaded' })
+    await homePage.gotoLogin()
+    await loginPage.login(USERNAME, PASSWORD)
+    await page.getByRole('link', { name: 'Arts' }).first().click()
+    await page.getByText('Upload Art').click()
+    await page.waitForTimeout(1000)
+    expect(page.locator('.uppy-Dashboard-AddFiles-title')).toBeVisible()
+  })
 
-test('should fill required fields for upload art', async ({ page }) => {
-  const loginPage = new LoginPage(page)
-  const homePage = new HomePage(page, 'kunsthalte')
+  test('TC03- should fill required fields for upload art', async ({ page }) => {
+    const loginPage = new LoginPage(page)
+    const homePage = new HomePage(page, 'kunsthalte')
 
-  await page.goto(homePage.url, { waitUntil: 'domcontentloaded' })
-  await homePage.gotoLogin()
-  await loginPage.login(USERNAME, PASSWORD)
-  await page.waitForTimeout(1000)
+    await page.goto(homePage.url, { waitUntil: 'domcontentloaded' })
+    await homePage.gotoLogin()
+    await loginPage.login(USERNAME, PASSWORD)
+    await page.waitForTimeout(1000)
 
-  await page.getByRole('link', { name: 'Arts' }).first().click()
-  await page.getByText('Upload Art').click()
-  await page.waitForTimeout(1000)
-  const filePath = 'apps/kunsthalte/public/arts.jpeg'
-  await page.locator('.uppy-Dashboard-input').first().setInputFiles(filePath)
-  await page.waitForTimeout(1000)
+    await page.getByRole('link', { name: 'Arts' }).first().click()
+    await page.getByText('Upload Art').click()
+    await page.waitForTimeout(1000)
+    const filePath = 'apps/kunsthalte/public/arts.jpeg'
+    await page.locator('.uppy-Dashboard-input').first().setInputFiles(filePath)
+    await page.waitForTimeout(1000)
 
-  await page.locator('#title').click()
-  await page.locator('#description').click()
-  await page.locator('#title').click()
+    await page.locator('#title').click()
+    await page.locator('#description').click()
+    await page.locator('#title').click()
 
-  expect(page.locator('.chakra-form__error-message').first()).toContainText(
-    'title is a required field',
-  )
-  expect(page.locator('.chakra-form__error-message').last()).toContainText(
-    'description is a required field',
-  )
-})
+    expect(page.locator('.chakra-form__error-message').first()).toContainText(
+      'title is a required field',
+    )
+    expect(page.locator('.chakra-form__error-message').last()).toContainText(
+      'description is a required field',
+    )
+  })
 
-test('The uploaded image should be displayed in the pending arts section.', async ({
-  page,
-}) => {
-  const loginPage = new LoginPage(page)
-  const homePage = new HomePage(page, 'kunsthalte')
+  test('TC04- The uploaded image should be displayed in the pending arts section', async ({
+    page,
+  }) => {
+    const loginPage = new LoginPage(page)
+    const homePage = new HomePage(page, 'kunsthalte')
 
-  await page.goto(homePage.url, { waitUntil: 'domcontentloaded' })
-  await homePage.gotoLogin()
-  await loginPage.login(USERNAME, PASSWORD)
-  await page.waitForTimeout(2000)
+    await page.goto(homePage.url, { waitUntil: 'domcontentloaded' })
+    await homePage.gotoLogin()
+    await loginPage.login(USERNAME, PASSWORD)
+    await page.waitForTimeout(2000)
 
-  await page.getByRole('link', { name: 'Arts' }).first().click()
-  await page.getByText('Upload Art').click()
-  await page.waitForTimeout(1000)
-  const filePath = 'apps/kunsthalte/public/arts.jpeg'
-  await page.locator('.uppy-Dashboard-input').first().setInputFiles(filePath)
-  await page.waitForTimeout(1000)
-  await page.locator('.uppy-DashboardContent-save').click()
+    await page.getByRole('link', { name: 'Arts' }).first().click()
+    await page.getByText('Upload Art').click()
+    await page.waitForTimeout(1000)
+    const filePath = 'apps/kunsthalte/public/arts.jpeg'
+    await page.locator('.uppy-Dashboard-input').first().setInputFiles(filePath)
+    await page.waitForTimeout(1000)
+    await page.locator('.uppy-DashboardContent-save').click()
 
-  const title = faker.internet.userName().toString()
+    const title = faker.internet.userName().toString()
 
-  await page.locator('#title').fill(title)
-  await page.locator('#description').fill('Description Test')
-  expect(page.locator("//*[@type='submit']")).toBeEnabled()
+    await page.locator('#title').fill(title)
+    await page.locator('#description').fill('Description Test')
+    expect(page.locator("//*[@type='submit']")).toBeEnabled()
 
-  await page.locator("//*[@type='submit']").click()
+    await page.locator("//*[@type='submit']").click()
 
-  expect(page.getByText('Art successfully submitted')).toBeVisible()
+    expect(page.getByText('Art successfully submitted')).toBeVisible()
 
-  await page.waitForTimeout(1000)
+    await page.waitForTimeout(1000)
 
-  await page.getByText('Go to my profile').click()
+    await page.getByText('Go to my profile').click()
+    await page.locator('.chakra-tabs__tab').last().click()
+    await page.waitForTimeout(5000)
 
-  await page.locator('.chakra-tabs__tab').last().click()
+    await page.getByText('Pending Arts').click()
+    await page.waitForTimeout(5000)
 
-  await page.waitForTimeout(5000)
-
-  await page.getByText('Pending Arts').click()
-
-  await page.waitForTimeout(5000)
-
-  expect(
-    page.locator('.chakra-aspect-ratio div div img').first(),
-  ).toHaveAttribute('src')
+    expect(
+      page.locator('.chakra-aspect-ratio div div img').first(),
+    ).toHaveAttribute('src')
+  })
 })
