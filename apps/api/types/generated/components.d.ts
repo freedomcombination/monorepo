81e6cc15666148d4ba26824bccfc5d632ad66f3a
7alip <talipaltas@gmail.com>
2024-07-14 19:39:56 +0200

    Update packages

--- apps/api/types/generated/components.d.ts
+++ apps/api/types/generated/components.d.ts
@@ -13,21 +13,19 @@ export interface FlowFlow extends Schema.Component {
   }
 }
 
-export interface CourseCurriculum extends Schema.Component {
-  collectionName: 'components_course_curricula'
+export interface FaqFaq extends Schema.Component {
+  collectionName: 'components_faq_faqs'
   info: {
-    displayName: 'CurriculumItem'
+    displayName: 'FaqLocale'
     description: ''
   }
   attributes: {
-    title_en: Attribute.String
-    title_tr: Attribute.String
-    title_nl: Attribute.String
-    description_en: Attribute.Text
-    description_nl: Attribute.Text
-    description_tr: Attribute.Text
-    instructor: Attribute.String
-    date: Attribute.DateTime
+    question_en: Attribute.String
+    question_tr: Attribute.String
+    question_nl: Attribute.String
+    answer_en: Attribute.Text
+    answer_tr: Attribute.Text
+    answer_nl: Attribute.Text
   }
 }
 
@@ -52,19 +50,21 @@ export interface ContactContact extends Schema.Component {
   }
 }
 
-export interface FaqFaq extends Schema.Component {
-  collectionName: 'components_faq_faqs'
+export interface CourseCurriculum extends Schema.Component {
+  collectionName: 'components_course_curricula'
   info: {
-    displayName: 'FaqLocale'
+    displayName: 'CurriculumItem'
     description: ''
   }
   attributes: {
-    question_en: Attribute.String
-    question_tr: Attribute.String
-    question_nl: Attribute.String
-    answer_en: Attribute.Text
-    answer_tr: Attribute.Text
-    answer_nl: Attribute.Text
+    title_en: Attribute.String
+    title_tr: Attribute.String
+    title_nl: Attribute.String
+    description_en: Attribute.Text
+    description_nl: Attribute.Text
+    description_tr: Attribute.Text
+    instructor: Attribute.String
+    date: Attribute.DateTime
   }
 }
 
@@ -72,9 +72,9 @@ declare module '@strapi/types' {
   export module Shared {
     export interface Components {
       'flow.flow': FlowFlow
-      'course.curriculum': CourseCurriculum
-      'contact.contact': ContactContact
       'faq.faq': FaqFaq
+      'contact.contact': ContactContact
+      'course.curriculum': CourseCurriculum
     }
   }
 }
