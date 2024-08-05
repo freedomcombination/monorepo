c6fea64d7b7055b288cb85ae9ff227ee2bfb91c2
mustafa budak <mbudak@gmail.com>
2024-08-05 22:22:42 +0200

    search button test

--- packages/playwright/src/tests/SeachButton.spec.ts
+++ packages/playwright/src/tests/SeachButton.spec.ts
@@ -2,7 +2,7 @@ import { test } from '@playwright/test'
 
 import { getVercelUrl } from '../utils'
 
-test.describe('Search Tests', () => {
+function gotoSearch() {
   test.beforeEach(async ({ page }) => {
     await page.setViewportSize({ width: 1920, height: 1080 })
 
@@ -11,42 +11,76 @@ test.describe('Search Tests', () => {
     await page.getByRole('link', { name: 'View Arts' }).click()
     await page.waitForTimeout(1000)
   })
+}
 
-  test('Search Test Pozitif', async ({ page }) => {
-    const searchInput = await page.$('.css-1xmacr9')
-
-    // Type "Future" in the search box
+test.describe('Seach Button Test', () => {
+  gotoSearch()
+  test('TC-0001 Happy flow', async ({ page }) => {
+    const searchInput = await page.$('.css-1xmacr9') // 01. Locate the search input box (CSS class: .css-1xmacr9).
     if (searchInput) {
-      await searchInput.fill('Future')
-      await page.waitForTimeout(5000) // wait 5 seconds
+      await searchInput.fill('Future') // 02. Type "Future" in the search box.
 
-      // Count search results
-      const searchResults = await page.$$('.masonry-grid_column') // Replace 'masonry-grid_column' with the search results CSS class
-      const numberOfResults = searchResults.length
+      await page.waitForTimeout(5000) // wait 5 seconds
+      const searchResults = await page.$$('.masonry-grid_column') // 03. Count search results (CSS class: .masonry-grid_column).
+      const numberOfResults = searchResults.length // Count search results
       if (numberOfResults > 0) {
-        console.log(`Number of search results: ${numberOfResults}`)
+        // 04. Verify that the number of search results is greater than 0.
+        console.log(`Number of search (Future) results: ${numberOfResults}`) // 05. Print the number of results to the console.
         await page.waitForTimeout(5000) // wait 5 seconds
       }
     }
   })
-
-  test('Search Test Negatif', async ({ page }) => {
-    const searchInput = await page.$('.css-1xmacr9')
-
-    // Clear the search box and type "Future1" in the search box
+  test('TC-0002 Negative flow', async ({ page }) => {
+    const searchInput = await page.$('.css-1xmacr9') // 01. Locate the search input box (CSS class: .css-1xmacr9).
     if (searchInput) {
-      await searchInput.fill('')
-      await searchInput.fill('Future1')
+      await searchInput.fill('') // 02. Clear the search box.
+      await searchInput.fill('Future1') // 03. Type "Future1" in the search box.
     } else {
       throw new Error('search Input not found')
     }
     await page.waitForTimeout(5000) // wait 5 seconds
 
-    // Count search results
-    const searchResults = await page.$$('.masonry-grid_column') // Replace 'masonry-grid_column' with the search results CSS class
-    const numberOfResults = searchResults.length
+    const searchResults = await page.$$('.masonry-grid_column') // 04. Count search results (CSS class: .masonry-grid_column).
+    const numberOfResults = searchResults.length // Count search results
     if (numberOfResults < 1) {
-      console.log(`Number of search results: ${numberOfResults}`)
+      // 05. Verify that the number of search results is less than 1.
+      console.log(`Number of search (Future1) results: ${numberOfResults}`) // 06. Print the number of results to the console.
+      await page.waitForTimeout(5000) // wait 5 seconds
+    }
+  })
+  test('TC-0003  Negative flow. Error Handling', async ({ page }) => {
+    const searchInput = await page.$('.css-1xmacr9') // 01. Locate the search input box (CSS class: .css-1xmacr9).
+    if (searchInput) {
+      await searchInput.fill('') // 02. Clear the search box.
+      await searchInput.fill('Future2') // 03. Type "Future2" in the search box.
+    } else {
+      throw new Error('search Input not found')
+    }
+    await page.waitForTimeout(5000) // wait 5 seconds
+
+    const searchResults = await page.$$('.masonry-grid_column') // 04. Count search results (CSS class: .masonry-grid_column).
+    const numberOfResults = searchResults.length // Count search results
+    if (numberOfResults < 1) {
+      // 05. Verify that the number of search results is less than 1.
+      console.log(`Number of search (Future2) results: ${numberOfResults}`) // 06.  If no results are found, an appropriate error message is displayed.
+      await page.waitForTimeout(5000) // wait 5 seconds
+    }
+  })
+  test('TC-0004  Happy flow. Filtering and Sorting', async ({ page }) => {
+    const searchInput = await page.$('.css-1xmacr9') // 01. Locate the search input box (CSS class: .css-1xmacr9).
+    if (searchInput) {
+      await searchInput.fill('') // 02. Clear the search box.
+      await searchInput.fill('Future') // 03. Type "Future" in the search box.
+    } else {
+      throw new Error('search Input not found')
+    }
+    await page.waitForTimeout(5000) // wait 5 seconds
+
+    const searchResults = await page.$$('.masonry-grid_column') // 04. Count search results (CSS class: .masonry-grid_column).
+    const numberOfResults = searchResults.length // Count search results
+    if (numberOfResults > 0) {
+      // 05. Verify that the number of search results is greater than 0.
+      console.log(`Number of search (Future) results: ${numberOfResults}`) // 06.  Allowing the search results to be filtered and sorted according to certain criteria.
       await page.waitForTimeout(5000) // wait 5 seconds
     }
     page.close()
--- packages/playwright/src/tests/example.spec.ts
+++ packages/playwright/src/tests/example.spec.ts
@@ -1,67 +1,60 @@
-
-import { expect, test, ElementHandle } from '@playwright/test' // Playwright test ve expect modüllerini dahil eder.
+import { expect, test } from '@playwright/test' // Playwright test ve expect modüllerini dahil eder.
 // 'has title' adlı bir test tanımlar.
 test('has title', async ({ page }) => {
-    // Tarayıcı penceresinin boyutunu 1920x1080 olarak ayarlar (tam ekran).
-    await page.setViewportSize({ width: 1920, height: 1080 });
+  // Tarayıcı penceresinin boyutunu 1920x1080 olarak ayarlar (tam ekran).
+  await page.setViewportSize({ width: 1920, height: 1080 })
 
+  // Playwright web sitesine gider.
+  await page.goto('https://playwright.dev/')
 
-    // Playwright web sitesine gider.
-    await page.goto('https://playwright.dev/');
+  // Sayfa başlığının "Playwright" içerdiğini doğrular.
+  await expect(page).toHaveTitle(/Playwright/)
 
-    // Sayfa başlığının "Playwright" içerdiğini doğrular.
-    await expect(page).toHaveTitle(/Playwright/);
+  // Sayfanın kapanmadan önce 5 saniye beklemesini sağlar.
+  // await page.waitForTimeout(5000);
 
-    // Sayfanın kapanmadan önce 5 saniye beklemesini sağlar.
-    // await page.waitForTimeout(5000);
+  // "Get started" isimli bağlantıya tıklar.
+  await page.getByRole('link', { name: 'Get started' }).click()
 
-    // "Get started" isimli bağlantıya tıklar.
-    await page.getByRole('link', { name: 'Get started' }).click();
+  // Sayfanın kapanmadan önce 5 saniye beklemesini sağlar.
+  await page.waitForTimeout(1000)
 
-    // Sayfanın kapanmadan önce 5 saniye beklemesini sağlar.
-    await page.waitForTimeout(1000);
+  // "Writing tests" yazan metne tıklar.
+  await page.click('text="Writing tests"')
 
-    // "Writing tests" yazan metne tıklar.
-    await page.click('text="Writing tests"');
+  // Sayfanın kapanmadan önce 5 saniye beklemesini sağlar.
+  await page.waitForTimeout(1000)
 
-    // Sayfanın kapanmadan önce 5 saniye beklemesini sağlar.
-    await page.waitForTimeout(1000);
+  // "Generating tests" yazan metne tıklar.
+  await page.click('text="Generating tests"')
 
-    // "Generating tests" yazan metne tıklar.
-    await page.click('text="Generating tests"');
+  // Sayfanın kapanmadan önce 5 saniye beklemesini sağlar.
+  await page.waitForTimeout(1000)
 
-    // Sayfanın kapanmadan önce 5 saniye beklemesini sağlar.
-    await page.waitForTimeout(1000);
+  // Sayfadaki .DocSearch.DocSearch-Button seçicisine sahip öğeyi bul
+  const docSearchButton = await page.$('.DocSearch.DocSearch-Button')
 
+  // Butona tıkla
+  if (docSearchButton) {
+    await docSearchButton.click()
+  } else {
+    throw new Error('DocSearch button not found')
+  }
 
-    // Sayfadaki .DocSearch.DocSearch-Button seçicisine sahip öğeyi bul
-    const docSearchButton = await page.$('.DocSearch.DocSearch-Button');
+  // 5 saniye bekle
+  await page.waitForTimeout(1000)
 
-    // Butona tıkla
-    if (docSearchButton) {
-        await docSearchButton.click();
-    } else {
-        throw new Error('DocSearch button not found');
-    }
+  // Sayfadaki .DocSearch-Input seçicisine sahip öğeyi bul
+  const searchInput = await page.$('.DocSearch-Input')
 
+  // Arama kutusuna "Run" yaz
+  if (searchInput) {
+    await searchInput.fill('aaaaaa')
+  } else {
+    throw new Error('search Input not found')
+  }
 
-
-    // 5 saniye bekle
-    await page.waitForTimeout(1000);
-
-    // Sayfadaki .DocSearch-Input seçicisine sahip öğeyi bul
-    const searchInput = await page.$('.DocSearch-Input');
-
-    // Arama kutusuna "Run" yaz
-    if (searchInput) {
-        await searchInput.fill('aaaaaa');
-    } else {
-        throw new Error('search Input not found');
-    }
-
-
-
-    // 5 saniye bekle
-    await page.waitForTimeout(1000);
-    page.close(); // Sayfa kapanır.
-});
+  // 5 saniye bekle
+  await page.waitForTimeout(1000)
+  page.close() // Sayfa kapanır.
+})
