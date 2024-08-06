4159ebcc689b037ebf4155719c12e196a1df7db1
7alip <talipaltas@gmail.com>
2024-08-07 00:58:13 +0200

    Revert ai package updates

--- apps/api/types/generated/components.d.ts
+++ apps/api/types/generated/components.d.ts
@@ -1,18 +1,5 @@
 import type { Schema, Attribute } from '@strapi/strapi'
 
-export interface FlowFlow extends Schema.Component {
-  collectionName: 'components_flow_flows'
-  info: {
-    displayName: 'flow'
-    icon: 'manyToMany'
-  }
-  attributes: {
-    title: Attribute.String
-    duration: Attribute.String
-    presenter: Attribute.String
-  }
-}
-
 export interface FaqFaq extends Schema.Component {
   collectionName: 'components_faq_faqs'
   info: {
@@ -26,6 +13,19 @@ export interface FaqFaq extends Schema.Component {
     answer_en: Attribute.Text
     answer_tr: Attribute.Text
     answer_nl: Attribute.Text
+  }
+}
+
+export interface FlowFlow extends Schema.Component {
+  collectionName: 'components_flow_flows'
+  info: {
+    displayName: 'flow'
+    icon: 'manyToMany'
+  }
+  attributes: {
+    title: Attribute.String
+    duration: Attribute.String
+    presenter: Attribute.String
   }
 }
 
@@ -71,8 +71,8 @@ export interface ContactContact extends Schema.Component {
 declare module '@strapi/types' {
   export module Shared {
     export interface Components {
-      'flow.flow': FlowFlow
       'faq.faq': FaqFaq
+      'flow.flow': FlowFlow
       'course.curriculum': CourseCurriculum
       'contact.contact': ContactContact
     }
