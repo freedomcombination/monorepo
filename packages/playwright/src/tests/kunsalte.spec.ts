fafc9977268e60d6240ea08ac03f57fd8158288f
safak <safaktpkaya@gmail.com>
2024-08-12 23:37:21 +0200

    Second test

--- packages/playwright/src/tests/kunsalte.spec.ts
+++ packages/playwright/src/tests/kunsalte.spec.ts
@@ -11,22 +11,31 @@ test('test', async ({ page }) => {
   await page.getByTestId('input-password').press('CapsLock')
   await page.getByTestId('input-password').fill('Test?123')
   await page.getByTestId('button-submit-login').click()
-  await page.getByRole('button', { name: 'TR' }).click();
-  await page.getByRole('heading', { name: 'Sanat Durağı' }).click();
-  await page.getByText('"Farklı renklerin buluşma').click();
-  await page.getByRole('link', { name: 'Eserlere Gözat' }).click();
-  await page.getByText('Kategoriler').click();
-  await page.getByRole('button', { name: 'NL' }).click();
-  await page.getByRole('link', { name: 'Kunsten' }).first().click();
-  await page.getByRole('link', { name: 'Collecties' }).first().click();
-  await page.getByRole('link', { name: 'Activiteiten' }).click();
-  await page.getByRole('link', { name: 'Over Ons' }).first().click();
-  await page.getByRole('link', { name: 'Contact' }).first().click();
-  await page.getByRole('button', { name: 'EN', exact: true }).click();
-  await page.getByRole('link', { name: 'Arts' }).first().click();
-  await page.getByText('Other').click();
-  await page.getByRole('link', { name: 'Collections' }).first().click();
-  await page.getByRole('link', { name: 'Activities' }).click();
-  await page.getByRole('link', { name: 'About Us' }).first().click();
-  await page.getByRole('link', { name: 'Contact' }).first().click();
-});
+  await expect(page.getByRole('button', { name: 'TR' })).toBeVisible()
+  await expect(page.getByRole('link', { name: 'Arts' }).first()).toBeVisible()
+  await expect(page.getByRole('heading', { name: 'Art Station' })).toBeVisible()
+  await expect(page.getByRole('link', { name: 'View Arts' })).toBeVisible()
+  await page.getByRole('button', { name: 'TR' }).click()
+  await expect(
+    page.getByRole('heading', { name: 'Sanat Durağı' }),
+  ).toBeVisible()
+  await expect(page.getByRole('link', { name: 'Eserlere Gözat' })).toBeVisible()
+  await page.getByRole('button', { name: 'TR' }).click()
+  await page.getByRole('heading', { name: 'Sanat Durağı' }).click()
+  await page.getByText('"Farklı renklerin buluşma').click()
+  await page.getByRole('link', { name: 'Eserlere Gözat' }).click()
+  await page.getByText('Kategoriler').click()
+  await page.getByRole('button', { name: 'NL' }).click()
+  await page.getByRole('link', { name: 'Kunsten' }).first().click()
+  await page.getByRole('link', { name: 'Collecties' }).first().click()
+  await page.getByRole('link', { name: 'Activiteiten' }).click()
+  await page.getByRole('link', { name: 'Over Ons' }).first().click()
+  await page.getByRole('link', { name: 'Contact' }).first().click()
+  await page.getByRole('button', { name: 'EN', exact: true }).click()
+  await page.getByRole('link', { name: 'Arts' }).first().click()
+  await page.getByText('Other').click()
+  await page.getByRole('link', { name: 'Collections' }).first().click()
+  await page.getByRole('link', { name: 'Activities' }).click()
+  await page.getByRole('link', { name: 'About Us' }).first().click()
+  await page.getByRole('link', { name: 'Contact' }).first().click()
+})
