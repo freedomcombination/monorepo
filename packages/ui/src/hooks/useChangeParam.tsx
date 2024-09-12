5b905a9aa8baff4524e260d69cc5e003745a9732
7alip <talipaltas@gmail.com>
2024-09-12 20:22:40 +0200

    Memoize change param functions

--- packages/ui/src/hooks/useChangeParam.tsx
+++ packages/ui/src/hooks/useChangeParam.tsx
@@ -1,3 +1,5 @@
+import { useCallback } from 'react'
+
 import { produce } from 'immer'
 import { isEmpty, isEqual } from 'lodash'
 import { useRouter } from 'next/router'
@@ -12,75 +14,87 @@ export type ChangeParamArgs = Record<
 export const useChangeParams = () => {
   const { query, push } = useRouter()
 
-  const changeParams = (args: ChangeParamArgs) => {
-    // In the case of query has empty string param which we want to remove,
-    // it should be removed from both the query and args
-    const cleanedQuery = cleanQuery(query, args)
-    const cleanedArgs = cleanArgs(args)
+  const cleanQuery = (query: ParsedUrlQuery, args: ChangeParamArgs) => {
+    return produce(query, draft => {
+      for (const key in args) {
+        const param = args[key]
 
-    // TODO: Remove existing page query if other params are changed
-
-    const newQuery = { ...cleanedQuery, ...cleanedArgs }
-
-    if (isEqual(query, newQuery)) {
-      return
-    }
-
-    push({ query: newQuery })
-  }
-
-  const changePage = (page: number) => {
-    changeParams({ page })
-  }
-
-  const changeCategories = (categories: string) => {
-    changeParams({ categories })
-  }
-
-  const changeSearch = (search: string) => {
-    changeParams({ q: search })
-  }
-
-  return { changeParams, changePage, changeCategories, changeSearch }
-}
-
-const cleanQuery = (query: ParsedUrlQuery, args: ChangeParamArgs) => {
-  return produce(query, draft => {
-    for (const key in args) {
-      const param = args[key]
-
-      if (isEmpty(param)) {
-        delete draft[key]
-      }
-    }
-
-    return draft
-  })
-}
-
-// When we provide null value for a param, remove it from the query
-const cleanArgs = (args: ChangeParamArgs) => {
-  return produce(args, draft => {
-    for (const key in draft) {
-      const param = draft[key]
-
-      if (isEmpty(param)) {
-        delete draft[key]
-      }
-
-      if (Array.isArray(param)) {
-        const filtered = param.filter(Boolean)
-
-        if (filtered.length) {
-          draft[key] = qs.stringify(filtered, {
-            encodeValuesOnly: true,
-          })
-        } else {
+        if (isEmpty(param)) {
           delete draft[key]
         }
       }
-    }
 
-    return draft
-  })
+      return draft
+    })
+  }
+
+  // When we provide null value for a param, remove it from the query
+  const cleanArgs = (args: ChangeParamArgs) => {
+    return produce(args, draft => {
+      for (const key in draft) {
+        const param = draft[key]
+
+        if (isEmpty(param)) {
+          delete draft[key]
+        }
+
+        if (Array.isArray(param)) {
+          const filtered = param.filter(Boolean)
+
+          if (filtered.length) {
+            draft[key] = qs.stringify(filtered, {
+              encodeValuesOnly: true,
+            })
+          } else {
+            delete draft[key]
+          }
+        }
+      }
+
+      return draft
+    })
+  }
+
+  const changeParams = useCallback(
+    (args: ChangeParamArgs) => {
+      // In the case of query has empty string param which we want to remove,
+      // it should be removed from both the query and args
+      const cleanedQuery = cleanQuery(query, args)
+      const cleanedArgs = cleanArgs(args)
+
+      // TODO: Remove existing page query if other params are changed
+
+      const newQuery = { ...cleanedQuery, ...cleanedArgs }
+
+      if (isEqual(query, newQuery)) {
+        return
+      }
+
+      push({ query: newQuery })
+    },
+    [query, push],
+  )
+
+  const changePage = useCallback(
+    (page: number) => {
+      changeParams({ page })
+    },
+    [changeParams],
+  )
+
+  const changeCategories = useCallback(
+    (categories: string) => {
+      changeParams({ categories })
+    },
+    [changeParams],
+  )
+
+  const changeSearch = useCallback(
+    (search: string) => {
+      changeParams({ q: search })
+    },
+    [changeParams],
+  )
+
+  return { changeParams, changePage, changeCategories, changeSearch }
 }
