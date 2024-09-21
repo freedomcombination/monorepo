4a5cef88a9ec901e985c6cba6475d6def8eda6e1
bbluechip <cs.orhan.berk@gmail.com>
2024-09-22 01:53:23 +0200

    PlatformsMenu coded, might contain bugs

--- packages/ui/src/components/PlatformsMenu/PlatformsMenu.tsx
+++ packages/ui/src/components/PlatformsMenu/PlatformsMenu.tsx
@@ -6,50 +6,79 @@ import {
   PopoverContent,
   PopoverHeader,
   PopoverBody,
-  PopoverFooter,
   PopoverArrow,
-  PopoverCloseButton,
-  Button,
+  IconButton,
+  SimpleGrid,
+  Box,
+  Image,
+  Link,
   Portal,
 } from '@chakra-ui/react'
+import { PiDotsNineBold } from 'react-icons/pi'
+
+import { useAuthContext } from '../../../../context/src/auth/useAuthContext';
 
 type PlatformsMenuProps = {
   authenticated?: boolean
-  public?: string
+  userRole?: string
 }
 
 export const PlatformsMenu: FC<PlatformsMenuProps> = ({
-  authenticated,
-  public,
+  userRole,
 }) => {
+  const { site } = useAuthContext()
+
+  if (
+    site !== 'foundation' ||
+    userRole === 'authenticated' ||
+    userRole === 'public'
+  ) {
+    return null
+  }
+
   return (
     <Popover>
       <PopoverTrigger>
-        <Button>Trigger</Button>
+        <IconButton
+          aria-label="Platforms Menu"
+          icon={<PiDotsNineBold />}
+          variant="ghost"
+        />
       </PopoverTrigger>
       <Portal>
-        <PopoverContent>
-          <PopoverHeader>Header</PopoverHeader>
+        <PopoverContent width="250px">
+          <PopoverArrow />
+          <PopoverHeader>Platforms</PopoverHeader>
           <PopoverBody>
-            <Button colorScheme="blue">Button</Button>
-          </PopoverBody>
-        </PopoverContent>
-        <PopoverContent>
-          <PopoverHeader>Header</PopoverHeader>
-          <PopoverBody>
-            <Button colorScheme="red">Button</Button>
-          </PopoverBody>
-        </PopoverContent>
-        <PopoverContent>
-          <PopoverHeader>Header</PopoverHeader>
-          <PopoverBody>
-            <Button colorScheme="yellow">Button</Button>
-          </PopoverBody>
-        </PopoverContent>
-        <PopoverContent>
-          <PopoverHeader>Header</PopoverHeader>
-          <PopoverBody>
-            <Button colorScheme="cyan">Button</Button>
+            <SimpleGrid columns={2} spacing={4}>
+              <Box textAlign="center">
+                <Link
+                  href="https://dashboard.freedomcombination.com"
+                  isExternal
+                >
+                  <Image src="https://dashboard.freedomcombination.com/images/foundation-logo.svg" alt="Foundation Logo" />
+                  Dashboard
+                </Link>
+              </Box>
+              <Box textAlign="center">
+                <Link href="https://kunsthalte.com" isExternal>
+                  <Image src="https://www.kunsthalte.com/images/kunsthalte-logo.svg" alt="Kunsthalte Logo" />
+                  Kunsthalte
+                </Link>
+              </Box>
+              <Box textAlign="center">
+                <Link href="https://lotusvdmedia.com" isExternal>
+                  <Image src="lotusvmedia.png" alt="Lotus Logo" />
+                  Lotus
+                </Link>
+              </Box>
+              <Box textAlign="center">
+                <Link href="https://trendrights.com" isExternal>
+                  <Image src="https://www.trendrights.com/images/trend-rights-logo-light.svg" alt="Trend Rights Logo" />
+                  Trend Rights
+                </Link>
+              </Box>
+            </SimpleGrid>
           </PopoverBody>
         </PopoverContent>
       </Portal>
