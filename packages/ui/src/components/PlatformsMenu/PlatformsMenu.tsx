66d646e82b53264302025be7d21117456ffc4175
bbluechip <cs.orhan.berk@gmail.com>
2024-09-21 09:03:20 +0200

    Created base skeleton of the PlatformsMenu component

---
+++ packages/ui/src/components/PlatformsMenu/PlatformsMenu.tsx
@@ -0,0 +1,39 @@
+import {FC} from 'react'
+
+import {
+    Popover,
+    PopoverTrigger,
+    PopoverContent,
+    PopoverHeader,
+    PopoverBody,
+    PopoverFooter,
+    PopoverArrow,
+    PopoverCloseButton,
+    Button,
+    Portal,
+  } from '@chakra-ui/react'
+
+type PlatformsMenuProps = {
+    
+}
+
+export const PlatformsMenu: FC<PlatformsMenuProps> = ({
+
+}) => {
+    <Popover>
+  <PopoverTrigger>
+    <Button>Trigger</Button>
+  </PopoverTrigger>
+  <Portal>
+    <PopoverContent>
+      <PopoverArrow />
+      <PopoverHeader>Header</PopoverHeader>
+      <PopoverCloseButton />
+      <PopoverBody>
+        <Button colorScheme='blue'>Button</Button>
+      </PopoverBody>
+      <PopoverFooter>This is the footer</PopoverFooter>
+    </PopoverContent>
+  </Portal>
+</Popover>
+}
