564c158ef5973a2a9f999cf34aac72664921ffa4
bbluechip <cs.orhan.berk@gmail.com>
2024-09-22 01:55:04 +0200

    yarn format

--- packages/ui/src/components/PlatformsMenu/PlatformsMenu.tsx
+++ packages/ui/src/components/PlatformsMenu/PlatformsMenu.tsx
@@ -16,16 +16,14 @@ import {
 } from '@chakra-ui/react'
 import { PiDotsNineBold } from 'react-icons/pi'
 
-import { useAuthContext } from '../../../../context/src/auth/useAuthContext';
+import { useAuthContext } from '../../../../context/src/auth/useAuthContext'
 
 type PlatformsMenuProps = {
   authenticated?: boolean
   userRole?: string
 }
 
-export const PlatformsMenu: FC<PlatformsMenuProps> = ({
-  userRole,
-}) => {
+export const PlatformsMenu: FC<PlatformsMenuProps> = ({ userRole }) => {
   const { site } = useAuthContext()
 
   if (
@@ -56,13 +54,19 @@ export const PlatformsMenu: FC<PlatformsMenuProps> = ({
                   href="https://dashboard.freedomcombination.com"
                   isExternal
                 >
-                  <Image src="https://dashboard.freedomcombination.com/images/foundation-logo.svg" alt="Foundation Logo" />
+                  <Image
+                    src="https://dashboard.freedomcombination.com/images/foundation-logo.svg"
+                    alt="Foundation Logo"
+                  />
                   Dashboard
                 </Link>
               </Box>
               <Box textAlign="center">
                 <Link href="https://kunsthalte.com" isExternal>
-                  <Image src="https://www.kunsthalte.com/images/kunsthalte-logo.svg" alt="Kunsthalte Logo" />
+                  <Image
+                    src="https://www.kunsthalte.com/images/kunsthalte-logo.svg"
+                    alt="Kunsthalte Logo"
+                  />
                   Kunsthalte
                 </Link>
               </Box>
@@ -74,7 +78,10 @@ export const PlatformsMenu: FC<PlatformsMenuProps> = ({
               </Box>
               <Box textAlign="center">
                 <Link href="https://trendrights.com" isExternal>
-                  <Image src="https://www.trendrights.com/images/trend-rights-logo-light.svg" alt="Trend Rights Logo" />
+                  <Image
+                    src="https://www.trendrights.com/images/trend-rights-logo-light.svg"
+                    alt="Trend Rights Logo"
+                  />
                   Trend Rights
                 </Link>
               </Box>
