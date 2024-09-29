37eca92a8973a42ea12b5ec4703cd7387c70b6f8
7alip <talipaltas@gmail.com>
2024-09-30 01:21:20 +0200

    Add mock functions

--- packages/mocks/common.ts
+++ packages/mocks/common.ts
@@ -2,10 +2,15 @@ import { type Faker } from '@faker-js/faker'
 import { faker as fakerEn } from '@faker-js/faker/locale/en'
 import { faker as fakerNl } from '@faker-js/faker/locale/nl'
 import { faker as fakerTr } from '@faker-js/faker/locale/tr'
-import { BlocksContent } from '@strapi/blocks-react-renderer'
+import { sample } from 'lodash'
 
-import { StrapiModelKeys } from '@fc/types'
-import { Localize, StrapiLocale } from '@fc/types'
+import {
+  ApprovalStatus,
+  Localize,
+  StrapiCollectionResponse,
+  StrapiLocale,
+  StrapiModel,
+} from '@fc/types'
 
 const faker: Localize<Faker> = {
   en: fakerEn,
@@ -13,53 +18,69 @@ const faker: Localize<Faker> = {
   tr: fakerTr,
 }
 
-export const generateNodeMock = (locale: StrapiLocale): BlocksContent => [
-  {
-    type: 'heading',
-    level: 1,
-    children: [{ type: 'text', text: faker[locale].lorem.sentence() }],
+export const generateMeta = (total: number, size = 25) => ({
+  pagination: {
+    page: 1,
+    pageSize: size,
+    pageCount: Math.ceil(total / size),
+    total,
   },
-  ...(Array.from({ length: 3 }, () => ({
-    type: 'paragraph',
-    children: [{ type: 'text', text: faker[locale].lorem.paragraph() }],
-  })) as BlocksContent),
-]
+})
 
-export const generateFieldMock = (
-  locale: StrapiLocale,
-  key: StrapiModelKeys,
-) => {
-  switch (key) {
-    case 'id':
-      return faker[locale].number.int()
-    case 'BIC':
-      return faker[locale].finance.bic()
-    case 'IBAN1':
-      return faker[locale].finance.iban()
-    case 'IBAN2':
-      return faker[locale].finance.iban()
-    case 'KVK':
-      return faker[locale].finance.accountNumber()
-    case 'RSIN':
-      return faker[locale].finance.accountNumber()
-    case 'about_en':
-    case 'about_nl':
-    case 'about_tr':
-      return generateNodeMock(locale)
-    case 'address':
-      return faker[locale].address.streetAddress()
-    case 'city':
-      return faker[locale].address.city()
-    case 'createdAt':
-    case 'updatedAt':
-    case 'publishedAt':
-      return faker[locale].date.recent().toISOString()
-    case 'description_en':
-    case 'description_nl':
-    case 'description_tr':
-      return faker[locale].lorem.paragraph({ min: 1, max: 3 })
+export const generateCommonFields = (locale: StrapiLocale) => {
+  const name = faker[locale].lorem.sentence({ min: 1, max: 4 })
+  const slug = faker[locale].helpers.slugify(name)
 
-    default:
-      return null
+  return {
+    id: faker.en.number.int(),
+    createdAt: faker.en.date.recent().toISOString(),
+    publishedAt: faker.en.date.recent().toISOString(),
+    updatedAt: faker.en.date.recent().toISOString(),
+    content: faker[locale].lorem.paragraphs({ min: 3, max: 10 }),
+    content_en: faker.en.lorem.paragraphs({ min: 3, max: 10 }),
+    content_nl: faker.nl.lorem.paragraphs({ min: 3, max: 10 }),
+    content_tr: faker.tr.lorem.paragraphs({ min: 3, max: 10 }),
+    description: faker[locale].lorem.paragraph(),
+    description_en: faker.en.lorem.paragraph(),
+    description_nl: faker.nl.lorem.paragraph(),
+    description_tr: faker.tr.lorem.paragraph(),
+    name,
+    slug,
+    title: name,
+    title_en: name,
+    title_nl: faker.nl.lorem.sentence(),
+    title_tr: faker.tr.lorem.sentence(),
+    approvalStatus: sample([
+      'approved',
+      'pending',
+      'rejected',
+    ]) as ApprovalStatus,
+    likes: faker.en.number.int({ max: 100 }),
+    views: faker.en.number.int({ max: 2000 }),
+  }
+}
+
+export const generateStrapiResponse = <T extends StrapiModel>(
+  fn: (locale: StrapiLocale) => T,
+  locale: StrapiLocale = 'en',
+  size = 25,
+): StrapiCollectionResponse<T[]> => {
+  const total = Math.floor(Math.random() * 12)
+  const length = total % size ?? total
+
+  return {
+    data: Array.from({ length }, () => fn(locale) as T),
+    meta: generateMeta(total, size),
+  }
+}
+
+export const generateLocalizedStrapiResponse = <T extends StrapiModel>(
+  fn: (locale: StrapiLocale) => T,
+  size = 25,
+): Localize<StrapiCollectionResponse<T[]>> => {
+  return {
+    tr: generateStrapiResponse(fn, 'tr', size),
+    nl: generateStrapiResponse(fn, 'nl', size),
+    en: generateStrapiResponse(fn, 'en', size),
   }
 }
---
+++ packages/mocks/faker.ts
@@ -0,0 +1,12 @@
+import { Faker } from '@faker-js/faker/.'
+import { faker as fakerEn } from '@faker-js/faker/locale/en'
+import { faker as fakerNl } from '@faker-js/faker/locale/nl'
+import { faker as fakerTr } from '@faker-js/faker/locale/tr'
+
+import { Localize } from '@fc/types'
+
+export const faker: Localize<Faker> = {
+  en: fakerEn,
+  nl: fakerNl,
+  tr: fakerTr,
+}
