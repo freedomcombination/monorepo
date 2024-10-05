5b7ff112e29402fc636b5d3de6bfc9b41e5f9e3e
burhan1997 <burhanelaldi97@gmail.com>
2024-10-05 18:42:32 +0200

    ex3 is completed

--- packages/ui/examples/E3_FetchWithFilters.tsx
+++ packages/ui/examples/E3_FetchWithFilters.tsx
@@ -8,18 +8,48 @@ import { strapiRequest } from '@fc/services/common/strapiRequest'
 type RequestFilterProps = {
   initialValue: string
 }
-
 export const FetchWithFilters: FC<RequestFilterProps> = ({ initialValue }) => {
-  const [blogs, setBlogs] = useState([])
+  const [blogs, setBlogs] = useState<any[]>([]) // State for blogs
   const [title, setTitle] = useState(initialValue)
 
   useEffect(() => {
-    // REF: https://docs.strapi.io/dev-docs/api/rest/filters-locale-publication#filtering
-    // TODO: Fetch blogs with by providing `title` filter to strapiRequest (title: { $containsi: title })
-    // NOTE: Changing `title` should trigger a new fetch.
-    // BONUS: You can play by using other arguments of strapiRequest (sort, populate,  fields etc).
-  }, [])
+    const fetchBlogs = async () => {
+      try {
+        const response = await strapiRequest({
+          endpoint: 'blogs',
+          locale: 'tr',
+          filters: {
+            title: {
+              $containsi: title, // Filter condition for title
+            },
+          },
+          sort: ['createdAt:desc'], // Sort by creation date descending
+        })
+        setBlogs(response.data || []) // Set the blogs from response
+      } catch (error) {
+        console.error('Error fetching blogs:', error)
+      }
+    }
 
-  // TODO Add Input to change titleFilter
-  return <Box>{/* TODO: Show only title of the blogs */}</Box>
+    if (title) fetchBlogs()
+  }, [title]) // Fetch when title changes
+
+  // Function to handle input changes
+  const handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.target.value)
+  }
+
+  return (
+    <Box>
+      <input
+        type="text"
+        placeholder="Filter by Title"
+        value={title}
+        onChange={handleTitleChange}
+      />
+      {blogs.map(blog => (
+        <div key={blog.id}>{blog.title}</div>
+      ))}
+    </Box>
+  )
 }
