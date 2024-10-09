ac5a732155302a1b40396a71e7dc14ee5aaea854
7alip <talipaltas@gmail.com>
2024-10-09 02:22:29 +0200

    Add test onboarding

---
+++ packages/playwright/src/onboarding/04-pom.spec.ts
@@ -0,0 +1,18 @@
+/// //////////////////////////////////////////////////////////////////// ///
+///                                                                      ///
+///                      04. PAGE OBJECT MODEL                           ///
+///            branch: onboarding/<username>/04-pom                      ///
+///                                                                      ///
+/// //////////////////////////////////////////////////////////////////// ///
+
+import { test } from '@playwright/test'
+
+// https://playwright.dev/docs/pom
+test.describe('OB-04. POM', { tag: ['@onboarding'] }, () => {
+  // TODO:
+  // Create a Page Object Model for the bol.com website (04-BolComPage.ts)
+  // - [ ] Create a page object for the home page
+  // - [ ] Define some locators and methods for the home page
+  // - [ ] Write the same test as in 01-write-test.spec.ts but using the POM
+  // - [ ] Bonus: Write more tests using the POM
+})
